818361431bb0bbecd4cebd8d51ecf31b9f60ae5a
Anusha <cdan.dharmasena@gmail.com>
2023-10-28 11:47:03 +1100

    oops bad build env var

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
-const buildNumber = isProduction ? process.env.BUILD_NUMBER : 'dev';
+const buildNumber = isProduction ? process.env.REACT_APP_BUILD_NUMBER : 'dev';
 
 export const config = {
   API_URI: 'https://crypto-stdev-express.vercel.app',
